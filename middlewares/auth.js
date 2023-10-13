da631bf67f54dfab5c9fa08c2c2390325dcbeafe
Magomed Musakadiev <musakadiev-mag@yandex.ru>
2023-10-13 20:30:08 +0300

    ser level-1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UNAUTHORIZED_ERROR = require('../errors/UnauthorizedError');
-console.log('auth токен в самом начале');
-console.log(jwt);
+
 let JWT_SECRET = '';
 if (process.env.NODE_ENV === 'production') { JWT_SECRET = process.env.JWT_SECRET; } else { JWT_SECRET = 'cibirkulimay'; }
 
 module.exports = (req, res, next) => {
+  console.log('req.cookies.jwt в самом начале');
+  console.log(req.cookies.jwt);
+  console.log(req);
   if (!req.cookies.jwt) next(new UNAUTHORIZED_ERROR(`Необходима авторизация //  test: ${process.env.TEST}`)); // проверяю подключен ли .env
+  console.log('константа jwt');
+  console.log(jwt);
   const token = req.cookies.jwt;
   let payload;
   try {
